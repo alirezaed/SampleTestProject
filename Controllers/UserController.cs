5875d003df811a11161018c053c8d0958b43e820
aedalat <a.edalatpour@gmail.com>
2023-03-05 11:25:27 +0330

    init

---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestProject.Services;
+
+namespace TestProject.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            var list = new List<User>();
+            list.Add(new User { Name = "Ahmad", Email = "ahmad@gmail.com" });
+            list.Add(new User { Name = "Reza", Email = "reza@gmail.com" });
+            list.Add(new User { Name = "Hosein", Email = "ahmad@gmail.com" });
+            list.Add(new User { Name = "Ahmad2", Email = "ahmad2@gmail.com" });
+            return Ok(list);
+        }
+    }
+}
